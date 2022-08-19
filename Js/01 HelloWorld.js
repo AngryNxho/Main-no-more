a4542320e8205c81492db6c7e985f8800164eeab
AngryNxho <pleasetrixcl@gmail.com>
2022-08-19 00:37:13 -0400

    hola

---
+++ Js/01 HelloWorld.js
@@ -0,0 +1,4 @@
+console.log("hello World");
+console.log([1,2,3]);
+console.log(5);
+console.log({key: 'value'});
---
+++ Js/02 Variables.js
@@ -0,0 +1,15 @@
+//var de variable es una palabra reservada
+var a = 15;
+console.log(a);
+
+// let es la manera moderna de utilizar var
+let b = 25;
+console.log(b);
+
+let f = 25, d = 30;
+console.log(f,d) 
+
+// Constante: significa que el valor no cambiara en el futuro, si se intenta cambiar, arrojara un error
+const c = 35;
+console.log(c);
+
