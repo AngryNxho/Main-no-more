0daf287c558800a21813d2cab521ea0b5acecc00
AngryNxho <pleasetrixcl@gmail.com>
2022-08-25 15:40:22 -0400

    l

---
+++ Js/24 Declaracion vs Expresion.js
@@ -0,0 +1,12 @@
+
+function sumar(a,b){
+    return a + b;
+}
+
+let sumar2 = function(a,b){
+    return a + b;
+}
+
+
+console.log(sumar(1,2), sumar2(1,2))
+// la function puede ser accedida antes de ser declarada, pero la expresion de una funcion  no
