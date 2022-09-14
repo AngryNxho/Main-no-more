e11ed0643310c0f2218d70b01ef2274af7c4b45d
AngryNxho <pleasetrixcl@gmail.com>
2022-09-14 16:52:36 -0300

    otra ve

---
+++ Programador X/Python/Funab/Funab/CursoPython/POO/leccion03/PruebaPersona.py
@@ -0,0 +1,9 @@
+from Persona import *
+
+persona1 = Persona("Carlos", 30, )
+
+print(persona1)
+
+empleado1 = Empleado("Karla", 29, 5000)
+print(empleado1)
+
---
+++ Programador X/Python/Funab/Funab/CursoPython/POO/leccion06/test_constante.py
@@ -0,0 +1,15 @@
+from constantes import MI_CONSTANTE, Matematicas as Mate
+
+print(MI_CONSTANTE)
+print(Mate.PI)
+
+
+#No debemos cambiar el valor de una constante
+
+# MI_CONSTANTE = 'Nuevo valor'
+
+# print(MI_CONSTANTE)
+
+
+
+    
