d71b140d120f07875ac7b4995b35dcb73c120c59
AngryNxho <pleasetrixcl@gmail.com>
2022-09-26 20:09:40 -0300

    olo

---
+++ Hola Mundo/Js/switch.js
@@ -0,0 +1,24 @@
+let a  = 2;
+switch(a) {
+    case 1: {
+        console.log(":(")
+        break;
+    }
+
+    case 2: {
+        console.log("(:")
+        break;
+    }
+
+    case 3: {
+        console.log("Hola")
+        break;
+    }
+
+    default: {
+        console.log("chao")
+        break;
+    }
+
+
+};
