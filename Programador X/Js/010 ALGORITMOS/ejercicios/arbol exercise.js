0668fefb519107ad75fded3fc08a442e32ff6f85
AngryNxho <pleasetrixcl@gmail.com>
2022-10-04 19:32:51 -0300

    a

---
+++ Programador X/Js/010 ALGORITMOS/ejercicios/arbol exercise.js
@@ -0,0 +1,26 @@
+class Node{
+    constructor(value, children = []){
+        this.value = value;
+        this.children = children;
+    }
+}
+
+
+let raiz = new Node(2, [
+    new Node(7,[
+        new Node(2),
+        new Node(10),
+        new Node(6, [
+            new Node(5),
+            new Node(11)
+        ])
+    ]),
+    new Node(5, [
+        new Node(9, [
+            new Node(4)
+        ])
+    ])
+])
+
+console.log(JSON.stringify(raiz, null, 2));
+
