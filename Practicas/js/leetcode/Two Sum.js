f213b68f3d99de1f56799afb7ed2c3f65f620fe2
AngryNxho <pleasetrixcl@gmail.com>
2022-10-04 18:39:26 -0300

    hola

--- Practicas/js/leetcode/Two Sum.js
+++ Practicas/js/leetcode/Two Sum.js
@@ -1,22 +1,17 @@
-function twoSum(numbers, result){
+function twoSum(nums, target){
+    suma = 0;
+    
     let lista = [];
-    let suma = 0;
-
-    for (let numero of numbers){
+    for (let numero  of nums){
         lista.push(numero);
-    }; 
-
-
-    for (let x = 0, y = 1; x <= lista.length && y < lista.length; x++, y++){
-        if (lista[x] + lista[y] == result){
-            return [x, y];
-        } 
-
     }
+
+    for (let numero in lista){
+        console.log(lista);
+    }
+
 }
 
 
-console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([3,2,4], 6));
-console.log(twoSum([3,3], 6));
-
+console.log(twoSum([2, 7, 11, 15], 9))
+// console.log(twoSum([3, 2, 3], 6)) 
