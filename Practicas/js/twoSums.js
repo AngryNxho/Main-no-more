f0062f8da2e805d6dca68d66f763da9acc4853d5
AngryNxho <pleasetrixcl@gmail.com>
2022-10-08 08:40:11 -0300

    the night we met

---
+++ Practicas/js/twoSums.js
@@ -0,0 +1,11 @@
+function twoSums(numeros, target = 0){
+    let lista = [];
+
+    for (let numero of arguments){
+        lista.push(numero)
+    }
+    return lista[0];
+}
+
+
+console.log(twoSums([1,2,3,4,], 3));
