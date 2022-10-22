b55d63a6c19480a0f86c99abd25d92d88035e55e
AngryNxho <pleasetrixcl@gmail.com>
2022-10-22 20:39:37 -0300

    maniac

---
+++ Cosas Variadas/Python/Curso Lucas.py/ejercicioLucas.py
@@ -0,0 +1,20 @@
+# Ejercicio
+# crea un programa que contenta dos variables, nombre y apellido
+# nombre=input("Escriba su nombre aqui:")
+# apellido=input("Escriba su apellido aqui:")
+# print("Encantado de conocerte",nombre, apellido)
+
+
+# Ejecicio 2
+# crea un programa que sume dos numeros y imprima el resultadoz
+# pizza=eval(input("Escriba un solo numero que quiere sumar:"))
+# pepsi=eval(input("Escriba el segundo numero que quiere sumar:"))
+# print(pizza + pepsi)
+
+# Ejercicio 3
+# crea un program que calcule que calcule tu edad
+
+
+añodenacimiento=eval(input("Escriba su año de nacimiento:"))
+año=eval(input("Escriba su año actual:"))
+print(año - añodenacimiento)
