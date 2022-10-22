e2983fdeb1c32118e3e49f92710d7faf9710d0a2
AngryNxho <pleasetrixcl@gmail.com>
2022-10-22 12:27:32 -0300

    aqa

--- Cosas Variadas/JavaScript/rotateString.js
+++ Cosas Variadas/JavaScript/rotateString.js
@@ -1,2 +1,22 @@
-let numero = Math.random();
-console.log(numero);
+
+function reverseString(stringg){
+    const string = stringg;
+    let length = string.length - 1;
+    let lista = [];
+
+    for (let i of string){
+        lista.push(string[length])
+        console.log(string[length]);
+        length--;
+    }
+
+    for (let x of lista){
+        let sumaString = '';
+        sumaString += x;
+    }   
+    
+    return lista;
+
+}
+
+console.log(reverseString('Hola Mundo'));
