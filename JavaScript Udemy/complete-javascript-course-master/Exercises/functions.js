98b94dc52d3ae649cad0ead5441fc09df30aa0db
AngryNxho <pleasetrixcl@gmail.com>
2022-10-22 20:53:54 -0300

    shes a maniac manica on the floor

---
+++ JavaScript Udemy/complete-javascript-course-master/Exercises/functions.js
@@ -0,0 +1,13 @@
+// function logger(){
+    // console.log("My name is Jonas");
+// }
+// 
+// logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`
+    console.log(juice);
+}
+fruitProcessor(3, 4);
+
