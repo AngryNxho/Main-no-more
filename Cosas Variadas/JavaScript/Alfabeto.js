6b6ff3663fedeba70829af7128fcb9b76ba07a40
AngryNxho <pleasetrixcl@gmail.com>
2022-10-24 17:17:24 -0300

    fdafdas

--- Cosas Variadas/JavaScript/Alfabeto.js
+++ Cosas Variadas/JavaScript/Alfabeto.js
@@ -1,20 +1,15 @@
+function ascii(valor) {
+  let letra = valor.charCodeAt();
 
-function ascii(valor){
-    
-    let letra = valor.charCodeAt();
+  letra += 1;
 
-    letra +=1;
-
-    if (127 > letra > 0) {
-        return  String.fromCharCode(letra);
-
-    }else{
-        return `has excedido el valor maximo`;
-    }
-};
-
+  if (127 > letra > 0) {
+    return String.fromCharCode(letra);
+  } else {
+    return `has excedido el valor maximo`;
+  }
+}
 
 console.log(ascii("y"));
 
-
-
+const x = 24;
