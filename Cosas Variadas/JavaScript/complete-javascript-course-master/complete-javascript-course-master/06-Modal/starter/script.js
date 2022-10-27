0aaaf48a0363f5480c93a00d8ad7d924b66aa9d8
AngryNxho <pleasetrixcl@gmail.com>
2022-10-27 20:26:57 -0300

    AAA

--- Cosas Variadas/JavaScript/complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ Cosas Variadas/JavaScript/complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenmodal = document.querySelectorAll('.show-modal');
+
+const closemodal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const OpenModal = function(){ 
+modal.classList.remove('hidden');
+overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsOpenmodal.length; i++)
+    btnsOpenmodal[i].addEventListener('click', OpenModal);
+
+    
+btnCloseModal.addEventListener('click', closemodal);
+overlay.addEventListener('click', closemodal);
--- freeMode/AdivinaMiNumero/script.js
+++ freeMode/AdivinaMiNumero/script.js
@@ -7,38 +7,40 @@
 let secretNumber = (Math.trunc(Math.random()* 20) + 1 );
 let userGuess = document.querySelector('.input').value;
 let score = 10;
+let highscore = 0;
 
 document.querySelector('.input').value = '';
+const displayMessage = function(message){
+    document.querySelector('.start').textContent = message; 
+}
+
 
 document.querySelector('.check').addEventListener('click', function(){
     let userGuess = Number(document.querySelector('.input').value);
 
     if (!userGuess){
-        document.querySelector('.start').textContent = 'No es un numero'
-    } else if (userGuess > secretNumber){
-        document.querySelector('.start').textContent = 'Muy Alto';
-        score--;
-        document.querySelector('.input').value = '';
-        document.querySelector('.score').textContent = score;
+        displayMessage('No es un numero')
+        
+    } else if (userGuess !== secretNumber){
         if (score < 1){
-            document.querySelector('.start').textContent = '😭 GAME OVER'
-            document.querySelector('.score').textContent = 0;
-
-        }
-    } else if (userGuess < secretNumber){
-        document.querySelector('.start').textContent = 'Muy Bajo';
-        score--;
-        document.querySelector('.input').value = '';
-        document.querySelector('.score').textContent = score;
-        if (score < 1){
-            document.querySelector('.start').textContent = '😭 GAME OVER'
-            document.querySelector('.score').textContent = 0;
+            displayMessage(userGuess > secretNumber ?'Muy Alto' : 'Muy Bajo')
+            score--;
+            document.querySelector('.input').value = '';
+            document.querySelector('.score').textContent = score;
+            if (score < 1){
+                displayMessage('😭 GAME OVER')
+                document.querySelector('.score').textContent = 0;
+            }
+                
         }
         
     } else if (userGuess === secretNumber){
         document.querySelector('.start').textContent = '🏆 Haz ganado!!!';
         document.querySelector('body').style.background = 'url(b5d55223b5fb572948d1e1e971ef4478.jpg)';
-
+        if (score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = score;
+        }
     }
 
 });
@@ -50,4 +52,5 @@ document.querySelector('.again').addEventListener('click', function(){
    document.querySelector('.score').textContent = 10;
    document.querySelector('body').style.background = 'url(https://c4.wallpaperflare.com/wallpaper/1015/933/121/abstract-grey-gray-minimalist-wallpaper-preview.jpg)';
    document.querySelector('.input').value = '';
+
 })
