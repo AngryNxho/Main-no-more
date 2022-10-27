fbd2daa705fb5afbfcfd38440c076655a5aa2665
AngryNxho <pleasetrixcl@gmail.com>
2022-10-26 22:22:58 -0300

    aa

--- freeMode/ToqueFama/script.js
+++ freeMode/ToqueFama/script.js
@@ -21,7 +21,7 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.input').value = '';
         document.querySelector('.score').textContent = score;
         if (score < 1){
-            document.querySelector('.start').textContent = 'GAME OVER'
+            document.querySelector('.start').textContent = '😭 GAME OVER'
             document.querySelector('.score').textContent = 0;
 
         }
@@ -31,7 +31,7 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.input').value = '';
         document.querySelector('.score').textContent = score;
         if (score < 1){
-            document.querySelector('.start').textContent = 'GAME OVER'
+            document.querySelector('.start').textContent = '😭 GAME OVER'
             document.querySelector('.score').textContent = 0;
         }
         
@@ -46,7 +46,7 @@ document.querySelector('.check').addEventListener('click', function(){
 document.querySelector('.again').addEventListener('click', function(){
    document.querySelector('.start').textContent = 'Empieza a adivinar';
    secretNumber =(Math.trunc(Math.random()* 20) + 1 );
-   score = 20
+   score = 10
    document.querySelector('.score').textContent = 10;
    document.querySelector('body').style.background = 'url(https://c4.wallpaperflare.com/wallpaper/1015/933/121/abstract-grey-gray-minimalist-wallpaper-preview.jpg)';
    document.querySelector('.input').value = '';
