f73b66893f3332dbc193e548a1c556bfe13129f6
AngryNxho <pleasetrixcl@gmail.com>
2022-10-30 19:31:37 -0300

    asdf

--- freeMode/PruebaToqueFama/script.js
+++ freeMode/PruebaToqueFama/script.js
@@ -5,8 +5,8 @@ const btnOpenModal = document.querySelector('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 // const playAgain = document.querySelector('.again');
 
-// btns
-let numberLength = (document.querySelector('.length'));
+// btns functions
+let numberLength = document.querySelector('.length');
 const startGame = document.querySelector('.start');
 const digits = document.querySelector('.digits');
 let checkBtn = document.querySelector('.check');
@@ -14,10 +14,7 @@ let randomNumber = Math.trunc(Math.random() * 9) + 1;
 let lista = [];
 let playAgain = document.querySelector('.again');
 // Info
-let changeDigits = function(){
-    largo = document.querySelector('.digits').textContent = numberLength.value;
-} 
-let  a = document.querySelector('.t');
+let largo = document.querySelector('.digits').textContent = numberLength.value;
 let counter;
 let vidas;
 const numberLoop = function(length) {
@@ -29,6 +26,8 @@ const numberLoop = function(length) {
     }
   }
   secretCode = lista
+  document.querySelector('.code').textContent = secretCode;
+  console.log(secretCode);
   return lista;
 };
 
@@ -47,50 +46,13 @@ nameSendButton.addEventListener('click', function () {
 
 btnOpenModal.addEventListener('click', openModal);
 btnCloseModal.addEventListener('click', closeModal);
-startGame.addEventListener('click', changeDigits);
-startGame.addEventListener('click', function(length) {
-  while (length.value > lista.length) {
-    if (lista.includes(randomNumber)) {
-      randomNumber = Math.trunc(Math.random() * 9) + 1;
-    } else if(!lista.includes){
-      lista.push(randomNumber);
-    }
-  }
-  document.querySelector('.code').textContent = numberLoop(largo);
-  // document.querySelector('.code').classList.add('hidden');
-  vidas = largo;
-  checkBtn.addEventListener('click', function(){
-
-    let userGuess = (document.querySelector('.userCode').value);
-  
-    for (let i in lista){
-      counter = 1
-
-      let z = 0;
-      let y = 1;
-      famas = document.querySelector('.famas'); 
-      toques = document.querySelector('.toques'); 
-      if (userGuess[i] == lista[i]){
-        counter++;
-        famas.textContent++;
-        toques.textContent++;
-        counter++;
-        document.querySelector;
-        if (famas.textContent == lista.length){
-            document.querySelector('.code').classList.remove('hidden');
-            document.querySelector('.winner').textContent = 'GANADOR';
-        }
-      } else if (lista.includes(Number(userGuess[i])) && userGuess[z] != userGuess[y]){
-        console.log(counter);        
-        z++;
-        y++;
-        toques.textContent++;
-      };
-    };
+startGame.addEventListener('click', function(){
+  document.querySelector('.winner'). textContent = `Debes adivinar un codigo con ${numberLength.value} Digitos`;
+  numberLoop(numberLength.value)
+  vidas = numberLength.textContent;
 });
 
  
-});
 playAgain.addEventListener('click', function(){
   famas.textContent = 0;
   toques.textContent = 0;
