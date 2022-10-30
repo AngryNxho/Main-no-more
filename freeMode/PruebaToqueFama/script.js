ac12b3cb10225bed6dcdcd2f45e92028eb228ce1
AngryNxho <pleasetrixcl@gmail.com>
2022-10-30 15:31:01 -0300

    asdff

--- freeMode/PruebaToqueFama/script.js
+++ freeMode/PruebaToqueFama/script.js
@@ -13,13 +13,13 @@ let checkBtn = document.querySelector('.check');
 let randomNumber = Math.trunc(Math.random() * 9) + 1;
 let lista = [];
 let playAgain = document.querySelector('.again');
-
 // Info
 let changeDigits = function(){
     largo = document.querySelector('.digits').textContent = numberLength.value;
 } 
-
-
+let  a = document.querySelector('.t');
+let counter;
+let vidas;
 const numberLoop = function(length) {
   while (length > lista.length) {
     if (lista.includes(randomNumber)) {
@@ -47,7 +47,7 @@ nameSendButton.addEventListener('click', function () {
 
 btnOpenModal.addEventListener('click', openModal);
 btnCloseModal.addEventListener('click', closeModal);
-startGame.addEventListener('click', changeDigits, );
+startGame.addEventListener('click', changeDigits);
 startGame.addEventListener('click', function(length) {
   while (length.value > lista.length) {
     if (lista.includes(randomNumber)) {
@@ -58,24 +58,36 @@ startGame.addEventListener('click', function(length) {
   }
   document.querySelector('.code').textContent = numberLoop(largo);
   // document.querySelector('.code').classList.add('hidden');
+  vidas = largo;
   checkBtn.addEventListener('click', function(){
+
     let userGuess = (document.querySelector('.userCode').value);
   
     for (let i in lista){
+      counter = 1
+
+      let z = 0;
+      let y = 1;
       famas = document.querySelector('.famas'); 
       toques = document.querySelector('.toques'); 
       if (userGuess[i] == lista[i]){
+        counter++;
         famas.textContent++;
         toques.textContent++;
+        counter++;
+        document.querySelector;
         if (famas.textContent == lista.length){
-          document.querySelector('.code').classList.remove('hidden');
-          document.querySelector('.winner').textContent = 'GANADOR';
+            document.querySelector('.code').classList.remove('hidden');
+            document.querySelector('.winner').textContent = 'GANADOR';
         }
-      } else if (lista.includes(Number(userGuess[i]))){
+      } else if (lista.includes(Number(userGuess[i])) && userGuess[z] != userGuess[y]){
+        console.log(counter);        
+        z++;
+        y++;
         toques.textContent++;
       };
     };
-  });
+});
 
  
 });
