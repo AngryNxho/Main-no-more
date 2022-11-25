19132a35dd6dd742b60e88bbd043883919331a51
AngryNxho <pleasetrixcl@gmail.com>
2022-11-25 06:00:06 -0300

    a

--- Programador X/Js/010 Estructuras de datos y Algoritmos/cola.js
+++ Programador X/Js/010 Estructuras de datos y Algoritmos/cola.js
@@ -1,32 +1,32 @@
-class Queue{
-    length = 0;
-    #first = 0;
+class Queue {
+  length = 0;
+  #first = 0;
 
-    enqueue(item){
-        this[this.length] = item;
-        this.length++;
-        return this.length;
+  enqueue(item) {
+    this[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  dequeue() {
+    let size = this.length - this.#first;
+    if (size == 0) {
+      return undefined;
     }
 
-    dequeue(){
-        let size = this.length - this.#first;
-        if (size == 0) {
-            return undefined;
-        }
+    const item = this[this.#first];
+    delete this[this.#first];
+    this.#first++;
+    return item;
+  }
 
-        const item =  this[this.#first];
-        delete this[this.#first];
-        this.#first++;
-        return item;
+  front() {
+    let size = this.length - this.#first;
+    if (size === 0) {
+      return undefined;
     }
-
-    front(){
-        let size = this.length - this.#first;
-        if (size === 0) {return undefined}
-        return this[this.#first];
-    }
-
-
+    return this[this.#first];
+  }
 }
 
 // let cola = new Queue();
@@ -34,7 +34,6 @@ class Queue{
 let cola = [];
 
 console.log(cola.length);
-
 
 cola.push('primero');
 console.log(cola[0]);
@@ -44,9 +43,6 @@ console.log(cola[0]);
 
 cola.push('tercero');
 console.log(cola[0]);
-
-
-
 
 console.log(cola.shift());
 console.log(cola.shift());
