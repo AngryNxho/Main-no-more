f5c14352385ee7d4d602e5025d3fd03ef6c7bd36
AngryNxho <pleasetrixcl@gmail.com>
2022-12-08 21:29:29 -0300

    vbh

--- freeMode/ListaDeCompras/script.js
+++ freeMode/ListaDeCompras/script.js
@@ -1,10 +1,16 @@
 const btn = document.querySelector('.submit');
 const content = document.querySelector('.content');
 let input = document.querySelector('input');
+const remove = document.querySelector('.remove');
+const check = document.querySelector('.check')
 
 btn.addEventListener('click', () => {
-  let elemento = document.createElement('li');
-  elemento.textContent = input.value;
-  content.insertAdjacentHTML('beforeend', elemento.textContent)
-
+  const element = document.createElement('p');
+  element.textContent = input.value;
+  content.appendChild(element);
 });
+
+
+
+
+
