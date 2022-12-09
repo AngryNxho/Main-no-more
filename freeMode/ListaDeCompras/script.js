3de0eeb2e007778df06e06de744333ba5dde80df
AngryNxho <pleasetrixcl@gmail.com>
2022-12-09 03:13:43 -0300

    no

--- freeMode/ListaDeCompras/script.js
+++ freeMode/ListaDeCompras/script.js
@@ -1,24 +1,27 @@
 const btn = document.querySelector('.submit');
 const content = document.querySelector('.content');
+const getBack = document.querySelector('.back');
 let input = document.querySelector('input');
-const btns = document.querySelectorAll('button');
-
+let array = [];
+let backup = [];
 btn.addEventListener('click', () => {
   const element = document.createElement('p');
   element.textContent = input.value;
   content.appendChild(element);
+  
   if (input.value.length > 0){
+    array.push(element.textContent);
     let del = document.createElement('button');
     del.textContent = 'delete'
     element.appendChild(del);
     element.classList.add('hidden')
+    
   }
+  input.value = '';
+  console.log(array, backup);
 });
 
 
-// for (let x = 0 ; x < btns.length; x++){
-  // 
-// };
 
 
 
