342d578ce66269f6ad113ceefb3dfce747563f16
AngryNxho <pleasetrixcl@gmail.com>
2022-12-08 23:41:18 -0300

    melon vino

--- freeMode/ListaDeCompras/script.js
+++ freeMode/ListaDeCompras/script.js
@@ -1,15 +1,24 @@
 const btn = document.querySelector('.submit');
 const content = document.querySelector('.content');
 let input = document.querySelector('input');
-const remove = document.querySelector('.remove');
-const check = document.querySelector('.check')
+const btns = document.querySelectorAll('button');
 
 btn.addEventListener('click', () => {
   const element = document.createElement('p');
   element.textContent = input.value;
   content.appendChild(element);
+  if (input.value.length > 0){
+    let del = document.createElement('button');
+    del.textContent = 'delete'
+    element.appendChild(del);
+    element.classList.add('hidden')
+  }
 });
 
+
+// for (let x = 0 ; x < btns.length; x++){
+  // 
+// };
 
 
 
