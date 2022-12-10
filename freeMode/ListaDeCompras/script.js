f6096c958d9112fac6ac2dfbab24a27403ebf5f6
AngryNxho <pleasetrixcl@gmail.com>
2022-12-10 14:51:32 -0300

    fgjh

--- freeMode/ListaDeCompras/script.js
+++ freeMode/ListaDeCompras/script.js
@@ -1,28 +1,41 @@
 const btn = document.querySelector('.submit');
 const content = document.querySelector('.content');
-const getBack = document.querySelector('.back');
+const deleteAll = document.querySelector('.clear');
 let input = document.querySelector('input');
 let array = [];
 let backup = [];
-btn.addEventListener('click', () => {
+
+const startShopping = function () {
   const element = document.createElement('p');
   element.textContent = input.value;
   content.appendChild(element);
-  
-  if (input.value.length > 0){
+  if (input.value.length > 0) {
     array.push(element.textContent);
     let del = document.createElement('button');
-    del.textContent = 'delete'
+    del.textContent = 'Ready';
     element.appendChild(del);
-    element.classList.add('hidden')
+    element.classList.add('hidden');
+
+    del.addEventListener('click', () => {
+      element.remove();
+    });
+
+    for (let x = 0; x < array.length; x++) {
+      deleteAll.addEventListener('click', () => {
+        element.remove();
+      });
+    }
+
     
   }
+
   input.value = '';
   console.log(array, backup);
+};
+btn.addEventListener('click', startShopping);
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter'){
+    console.log('Enter was pressed');
+    startShopping();
+  };
 });
-
-
-
-
-
-
