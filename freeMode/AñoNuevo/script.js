e19bbd36354adfb5b4b8e424187723d53d1f17ad
AngryNxho <pleasetrixcl@gmail.com>
2022-12-23 09:38:39 -0300

    a

--- freeMode/AñoNuevo/script.js
+++ freeMode/AñoNuevo/script.js
@@ -1,37 +1,55 @@
 const inputName = document.getElementById('name');
 const btn = document.querySelector('.btn');
 const content = document.querySelector('.content');
+const image = document.querySelector('.image');
+const fran = document.querySelector('.fran');
+const lucas = document.querySelector('.lucas');
+const esteban = document.querySelector('.papa');
+const mama = document.querySelector('.mama');
+const yo = document.querySelector('.yo');
+let name = [];
+let array = ['Ignacio', 'Alejandra', 'Esteban', 'Lucas', 'Francisca'];
 btn.addEventListener('click', () => {
-    if (inputName.value == 'Ignacio'){
-        let message = document.createElement('p');
-        message.textContent = 'Bienvenido Ignacio';                                   
-        content.appendChild(message);
-        location.href = 'ignacio.html'
-    }else if (inputName.value == 'Francisca'){
-        let message = document.createElement('p');
-        message.textContent = 'Bienvenida Francisca';                                   
-        content.appendChild(message);
-        location.href = 'francisca.html'
-    }else if (inputName.value == 'Alejandra'){
-        let message = document.createElement('p');
-        message.textContent = 'Bienvenida Alejandra';                                   
-        content.appendChild(message);
-        location.href = 'alejandra.html'
-    }else if (inputName.value == 'Lucas'){
-        let message = document.createElement('p');
-        message.textContent = 'Bienvenido Lucas';
-        location.href = 'lucas.html'
-        content.appendChild(message);
-    }else if (inputName.value == 'Esteban'){
-        let message = document.createElement('p');
-        message.textContent = 'Bienvenido Esteban';                                   
-        content.appendChild(message);
-        location.href = 'esteban.html'
+  if (array.includes(inputName.value)) {
+    let message = document.createElement('p');
+    message.textContent = `Bienvenido/a ${inputName.value}`;
+    content.appendChild(message);
+    name.push(inputName.value);
 
-    }
+    setTimeout(() => {
+      content.classList.add('hidden');
+      btn.classList.add('hidden');
+      inputName.classList.add('hidden');
+      image.classList.remove('hidden');
+    }, 2500);
 
-
+    image.addEventListener('click', () => {
+      if (name[name.length - 1] == 'Francisca') {
+        image.classList.add('hidden');
+        setTimeout(() => {
+          fran.classList.remove('hidden');
+        }, 1000);
+      } else if (name[name.length - 1] == 'Lucas') {
+        image.classList.add('hidden');
+        setTimeout(() => {
+          lucas.classList.remove('hidden');
+        }, 1000);
+      } else if (name[name.length - 1] == 'Esteban') {
+        image.classList.add('hidden');
+        setTimeout(() => {
+          esteban.classList.remove('hidden');
+        }, 1000);
+      } else if (name[name.length - 1] == 'Alejandra') {
+        image.classList.add('hidden');
+        setTimeout(() => {
+          mama.classList.remove('hidden');
+        }, 1000);
+      } else if (name[name.length - 1] == 'Ignacio') {
+        image.classList.add('hidden');
+        setTimeout(() => {
+          yo.classList.remove('hidden');
+        }, 1000);
+      }
+    });
+  }
 });
-
-
-
