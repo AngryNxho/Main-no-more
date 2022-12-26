c018ee2340bc706c9ee04ee20b67d17400bc1989
AngryNxho <pleasetrixcl@gmail.com>
2022-12-26 13:07:01 -0300

    a

---
+++ freeMode/PigGame/script.js
@@ -0,0 +1,10 @@
+const restartGame = document.querySelector('.newGame');
+const roll = document.querySelector('.roll');
+const hold = document.querySelector('.hold');
+const innerColor = document.querySelector('.innerColor');
+const innerColorTwo = document.querySelector('.innerColorTwo');
+let playerOne = document.querySelector('.pointsOne');
+
+roll.addEventListener('click', () =>{
+    playerOne.textContent = 20
+})
