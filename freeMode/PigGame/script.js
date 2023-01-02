d98ef3e2af65c13488c20c5fd33e28dff5d3d7d5
AngryNxho <pleasetrixcl@gmail.com>
2023-01-02 20:07:06 -0300

    aa

--- freeMode/PigGame/script.js
+++ freeMode/PigGame/script.js
@@ -5,26 +5,22 @@ const innerColor = document.querySelector('.innerColor');
 const innerColorTwo = document.querySelector('.innerColorTwo');
 let playerOne = document.querySelector('.pointsOne');
 let playerTwo = document.querySelector('.pointsTwo');
+let scoreOne = document.querySelector('.ScoreOne');
 const dice = document.querySelector('.dice');
 const dice2 = document.querySelector('.dice2');
 const dice3 = document.querySelector('.dice3');
 const dice4 = document.querySelector('.dice4');
 const dice5 = document.querySelector('.dice5');
 const dice6 = document.querySelector('.dice6');
-
-roll.addEventListener('click', () =>{ 
-  
-  while (playerOne.textContent != '1'){
-    playerOne.textContent
-    if (playerOne.textContent == '1'){
-      innerColor.classList.add('hidden');
-      innerColorTwo.classList.remove('hidden');
+let backup = [];
+let x = [];
+roll.addEventListener('click', () =>{
+    playerOne.textContent = Math.round(Math.random () * 5) + 1;
+    backup.push(playerOne.textContent);
+    for (let i = 0; i < backup.length; i++){
+        i
+        x += backup[i];
+        scoreOne.textContent = x;   
+        console.log(backup[i]);
     }
-  
-  }
-    
- 
-
-  
-
 });
