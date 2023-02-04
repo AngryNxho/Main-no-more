d01cffaa522c71699ac1df104683dea16a852de2
AngryNxho <pleasetrixcl@gmail.com>
2023-02-04 14:30:46 -0300

    asd

---
+++ Programador X/React/Z React Yo/Proyecto/metasapp/src/componentes/compartidos/Pie.js
@@ -0,0 +1,11 @@
+function Pie() {
+  return (
+    <footer className= "text-3xl font-bold underline">
+        <small>
+            Desarrolado por Xavier Reyes Ochoa ©2030. Todos los derechos reservados.
+        </small>
+    </footer>
+  );
+}
+
+export default Pie;
