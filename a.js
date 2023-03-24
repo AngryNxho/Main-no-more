0e3c2f67de8da684bf73a134a47cd46421c22a9f
AngryNxho <pleasetrixcl@gmail.com>
2023-03-23 21:56:29 -0300

    DSA

--- a.js
+++ a.js
@@ -1,28 +1,3 @@
-function reverseArr(arg){
+a = ['WE THE BEST MUSIC']
 
-    let arr = [];
-
-    for (let x = 0; x < arguments.length; x++){
-        arr.push(arguments[x]);
-    }
-
-    arrCopy = arr.map((z) => z)
-
-
-    c = 0;
-    i = 0;
-
-
-    arrCopy = arrCopy.reverse();
-
-    arr.forEach(e => {
-        if (e == arrCopy[c]){
-            i++;
-            console.log(i);
-        }
-    })
-
-}
-
-
-console.log(reverseArr(1,3,4));
+console.log(a.reverse( ));
