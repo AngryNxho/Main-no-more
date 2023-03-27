599effea5333a290ea7f9866a452254b30746fe1
AngryNxho <pleasetrixcl@gmail.com>
2023-03-27 16:06:02 -0300

    a

--- ReactJS/Holas/hola/src/DisplayMovie.js
+++ ReactJS/Holas/hola/src/DisplayMovie.js
@@ -4,16 +4,12 @@ function DisplayMovie({title, date}){
 
     const [counter, setCounter] = useState(0);
 
-    const handleClick = () => {
-        setCounter(counter + 1);
-    }
-
+    
     return (
         <div>
+            <button >Click</button>
             <p> The movie is: {title}</p>
             <p> The release date was on: {date}</p>
-            <button onClick={handleClick}>Click</button>
-            <p>Counter: {counter}</p>
         </div>
     )
 }
