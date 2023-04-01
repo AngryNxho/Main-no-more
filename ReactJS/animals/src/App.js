c5835bb514883505bad149f0e1856ae3bb690af8
AngryNxho <pleasetrixcl@gmail.com>
2023-04-01 15:46:12 -0300

    sdfgdfsg

--- ReactJS/animals/src/App.js
+++ ReactJS/animals/src/App.js
@@ -1,21 +1,30 @@
 import DisplayAnimals from "./DisplayAnimals";
 import {useState} from 'react';
 
+
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+
 function App() {
     
-    const [count, setCount] = useState(0);
+    const [animals, setAnimal] = useState([]);
 
     const handleClick = () => {
-        setCount(count + 1);
-    }
+        setAnimal([...animals, getRandomAnimal()])
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <DisplayAnimals type={animal} key={index}/>
+    })
 
     return (
         <div>
             <button onClick={handleClick}>Add animal</button>
-            <br />
-            <div>Animals to Show: {count}</div>
-
-            
+            <div>{renderedAnimals}</div>            
         </div>
     );
 }
