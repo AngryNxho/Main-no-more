f88a68c28d6df79b433b4fc8a49a67c739c8b42c
AngryNxho <pleasetrixcl@gmail.com>
2023-04-06 18:26:00 -0400

    xxcb

--- WorL.js
+++ WorL.js
@@ -1,15 +1,13 @@
-const Data = {
+const Description = {
     name:"AngryNxho",
     age: 21,
     aboutMe:"Not much really, just a guy who likes to spend time on the computer, and play some games",
     goal:"Find something that gives some purpose to my life",
     Display(){
-        return (`-Hello My name is ${Data.name}
-        \n-I am a ${Data.age} yo
-        \n-${Data.aboutMe}
-        \n-Rn my goal is to ${Data.goal}`);
+        return (`-Hello My name is ${Description.name}
+        \n-I am a ${Description.age} yo
+        \n-${Description.aboutMe}
+        \n-Rn my goal is to ${Description.goal}`);
     }
 };
-
-
-console.log(Data.Display());
+console.log(Description.Display());
