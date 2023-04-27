50d9232af28b55a5016cca6c007724171bf87f6b
AngryNxho <pleasetrixcl@gmail.com>
2023-04-27 07:04:40 -0400

    me quiero cortar la pija

--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -1,46 +1,29 @@
 function twoSum(nums, target){
 
   let index = [];  
-  for ([x, i, z, y] = [0, 1, 1, 2]; i < nums.length; i++){
-    if ((nums.length >= 2 && 
-      nums.length <= 10**4) &&
-      (nums[i] >= -(10**9) && 
-      nums[i] <= 10**9) &&
-      (target >= -(10**9) && 
-      target <= 10**9)){
-        op = nums[x] + nums[i];
-        if (op == target){
+  for ([x, i, z] = [0, 1, 1]; i < nums.length; i++){
+    if (nums[0] + nums[i] == target){
+      index.push(0, i);
+    }else if (nums[x] + nums[i] != target){
+      x = i - 1;
+      if (nums[x] + nums[i] == target){
+        index.push(x, i)
+      }else{
+        x--;
+        if (nums[x] + nums[i] == target ){
           index.push(x, i);
-
-        }else if (op != target && i <= 3){
-         if (op != target){
-           x++;
-         }
-          if  (nums[x] + nums[i] == target){
-            x = i - 1;
-            index.push(x, i);
-          }
-          
         }
-      }
-        
-        // else if(op != target){
-          // if (i < nums.length){
-            // x = 2
-          // }
-        // }else{
-          // console.log(2);
-        // }
-      // }
-    };
-    return index;
+    }
+    
+  }
 }
-
+  return index;
+}
 console.log(twoSum([2,7,11,15], 9)) // 0, 11
 console.log(twoSum([3,2,3], 6));// 0, 2
 console.log(twoSum([3,3], 6));// 0, 1
 console.log(twoSum([3,2,4], 6));// 1, 2
-console.log(twoSum([-1, -2, -3, -4, -5], -8));//2, 4
-console.log(twoSum([6,5,7,8,9,3], 10));// 2, 5
-
+console.log(twoSum([-1, -2, -3, -4, -5], -8));//2, 4 \\ 6
+console.log(twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 7)); //5, 11
+console.log(twoSum([6,5,7,8,9,3], 10));// 2, 5 \\ 7
 
--- WorL.js
+++ WorL.js
@@ -1,27 +1,7 @@
-// if (nums[y] + nums[i] == target){
-  // index.push(y, i);
-// 
-//  }else if (nums[x] + nums[i] != target){
-  //  x = (i - 1)
-  //  if (nums[x] + nums[i] == target){
-    //  index.push(x, i);
-  //  }
-//  }else if (index.length != target){
-  //  index.push(x, i);
-//  }
-// 
-if (nums[x] + nums[i] == target){
-  index.push(x, i);
-    x++;
-  }else if (nums[x] + nums[i] != target){
-  x = i - 1;
-  if (nums[x] + nums[i] == target){
-      index.push(x, i);
-  }
-}
-console.log(twoSum([2,7,11,15], 9)) // 0, 1
+console.log(twoSum([2,7,11,15], 9)) // 0, 11
 console.log(twoSum([3,2,3], 6));// 0, 2
 console.log(twoSum([3,3], 6));// 0, 1
 console.log(twoSum([3,2,4], 6));// 1, 2
-console.log(twoSum([-1, -2, -3, -4, -5], -8));//2, 4
-console.log(twoSum([6,5,7,8,9,3], 10));// 2, 5
+console.log(twoSum([-1, -2, -3, -4, -5], -8));//2, 4 \\ 6
+console.log(twoSum([6,5,7,8,9,3], 10));// 2, 5 \\ 7
+
