235393164c9caa41129208122b9280b946ae5bc3
AngryNxho <pleasetrixcl@gmail.com>
2023-04-27 16:18:04 -0400

    g

--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -1,29 +1,25 @@
 function twoSum(nums, target){
 
   let index = [];  
-  for ([x, i, z] = [0, 1, 1]; i < nums.length; i++){
-    if (nums[0] + nums[i] == target){
-      index.push(0, i);
-    }else if (nums[x] + nums[i] != target){
-      x = i - 1;
-      if (nums[x] + nums[i] == target){
-        index.push(x, i)
-      }else{
-        x--;
-        if (nums[x] + nums[i] == target ){
-          index.push(x, i);
+
+  for (let x in nums){
+    for (let i = 0; i < nums.length; i++){
+      if (nums[i] + nums[x] == target && nums[i] != nums[x]){
+        // console.log(`${nums[x]} + ${nums[i]} = ${nums[i] + nums[x]}`);
+        if (index.length <= 1){
+          index.push(x, i)
         }
+      }
     }
-    
   }
-}
+  
   return index;
 }
-console.log(twoSum([2,7,11,15], 9)) // 0, 11
+
+console.log(twoSum([2,7,11,15], 9)) // 0, 1
 console.log(twoSum([3,2,3], 6));// 0, 2
 console.log(twoSum([3,3], 6));// 0, 1
 console.log(twoSum([3,2,4], 6));// 1, 2
 console.log(twoSum([-1, -2, -3, -4, -5], -8));//2, 4 \\ 6
-console.log(twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 7)); //5, 11
+console.log(twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 11)); //5, 11
 console.log(twoSum([6,5,7,8,9,3], 10));// 2, 5 \\ 7
-
--- WorL.js
+++ WorL.js
@@ -1,7 +1,16 @@
-console.log(twoSum([2,7,11,15], 9)) // 0, 11
-console.log(twoSum([3,2,3], 6));// 0, 2
-console.log(twoSum([3,3], 6));// 0, 1
-console.log(twoSum([3,2,4], 6));// 1, 2
-console.log(twoSum([-1, -2, -3, -4, -5], -8));//2, 4 \\ 6
-console.log(twoSum([6,5,7,8,9,3], 10));// 2, 5 \\ 7
+let arr = [6,5,7,8,9,3];
+let index = [];
 
+
+for (let x of arr){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] + x == 10 && arr[i] != x){
+            console.log(`${arr[i]} + ${x} = ${x + arr[i]}`);
+            index.push(i)
+            index.sort()
+        }
+        
+    }
+}
+
+console.log(index);
