1f9837e2e02fa0c5c371989f08f709209fa131c2
AngryNxho <108508244+AngryNxho@users.noreply.github.com>
2023-06-29 02:04:19 -0400

    asdf

--- WorL.js
+++ WorL.js
@@ -1,16 +1,1 @@
-let arr = [6,5,7,8,9,3];
-let index = [];
-
-
-for (let x of arr){
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] + x == 10 && arr[i] != x){
-            console.log(`${arr[i]} + ${x} = ${x + arr[i]}`);
-            index.push(i)
-            index.sort()
-        }
-        
-    }
-}
-
-console.log(index);
+for (let i = 0; i < capacity.length; i++){console.log(2)}
--- a.js
+++ a.js
@@ -1,9 +1,24 @@
-function reverseArr(arg){
-    const string = [];
+function company(capacity, weight) {
+  bigBox = 210;
+  midBox = 90;
 
-    for (let x = 0;)
+  bigQuantity = 0;
+  midQuantity = 0;
+
+  for (let i = 0; i < capacity; i++) {
+    if (capacity >= bigBox) {
+      capacity -= bigBox;
+      bigQuantity += 1;
+    } else if (capacity < bigBox) {
+      capacity -= midBox;
+      midQuantity += 1;
+    }
+  }
+
+  return `Medium ${midQuantity} - Big ${bigQuantity} with a total of ${
+    midQuantity * midBox + bigQuantity * bigBox
+  }`;
 }
 
-
-
-console.log(reverseArr('Hola'));
+// console.log(company(500, 100));
+console.log(company(1500, 0));
