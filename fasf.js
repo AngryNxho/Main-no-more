08755c2115cfa78fe04c1efd7bff0726e462f1b1
AngryNxho <pleasetrix@gmail.com>
2023-07-01 17:48:22 -0400

    Logistic

---
+++ fasf.js
@@ -0,0 +1,4 @@
+console.log(510/210);
+console.log((510-420)/90)
+
+console.log(20*21);
--- wadahell.js
+++ wadahell.js
@@ -1,70 +1,45 @@
-function LogistiCompany(capacity, weigth) {
-  const max = capacity;
+function logistic(capacity, weight, user){
+    
+    let truckMax = capacity;
+    let weightUser = weight;
+    let userKg = user
 
-  const bigBox = 210;
-  const medBox = 90;
 
-  let bigQ = 0;
-  let mediumQ = 0;
+    const bigBox = 210;
+    const smallBox = 90;
 
-  for (let i = 0; i <= capacity; i++) {
-    if (bigBox <= capacity) {
-      capacity -= bigBox;
-      bigQ += 1;
-    } else if (medBox <= capacity) {
-      capacity -= medBox;
-      mediumQ += 1;
+    
+    let quantityBig = 0;
+    let quantitySmall=0;
+    
+    let totalMax = Math.round(capacity/weight)
+    
+    for (let i = 0; i < truckMax; i++){
+        if (truckMax > weightUser){
+            truckMax -= bigBox;
+            quantityBig += 1;
+        }else{
+            truckMax -= smallBox;
+            quantitySmall += 1;
+        }
     }
-  }
 
-  bigSum = bigBox * bigQ;
-  midSum = mediumQ * medBox;
-  total = bigSum + midSum;
-  // console.log(bigSum, midSum, bigSum + midSum);
-  // console.log(bigSum/weigth);
-  // console.log(midSum/weigth);
 
-  if (total <= capacity == false) {
-    console.log(max, total);
-    return `Big boxes: ${bigQ} (Kg) - Small Boxes: ${mediumQ}`;
-  } else {
-    return `Not enough space on this truck. Maximun capacity is: ${capacity} and the weitgh is ${weigth}`;
-  }
+    sumA =  (quantityBig * bigBox);
+    sumB =  (quantitySmall * smallBox);
+
+    let total = (quantityBig * bigBox) + (quantitySmall * smallBox);
+
+    // console.log(total);
+    // console.log(capacity);
+
+    if (total <= capacity){
+        return `Cajas Grandes: ${Math.round(quantityBig)} ${(Math.round(bigBox * quantityBig)/userKg)} Kilogramos(c/u) - Cajas Medianas: ${(quantitySmall)} ${Math.round((smallBox * quantitySmall)/userKg)} Kilogramos(c/u) de los ${capacity} y usaste ${total}`
+    }else{
+        return `No hay suficiente espacio en el camion, el limite es de ${capacity} y llevas ${total} Cajas grandes: ${quantityBig} - Cajas medianas: ${quantitySmall}`
+    }
+
 }
 
-console.log(LogistiCompany(1000, 750));
 
-
-// function calcularCajas(capacidadMaxima, pesoPedido) {
-//   const pesoCajaMediana = 90;
-//   const pesoCajaGrande = 210;
-
-//   if (pesoPedido > capacidadMaxima) {
-//     return "El camión no puede transportar el pedido.";
-//   }
-
-//   const cantidadCajasGrande = Math.floor(pesoPedido / pesoCajaGrande);
-//   const cantidadCajasMediana = Math.floor(
-//     (pesoPedido % pesoCajaGrande) / pesoCajaMediana
-//   );
-
-//   const capacidadRestante = pesoPedido - (cantidadCajasGrande * pesoCajaGrande) - (cantidadCajasMediana * pesoCajaMediana);
-
-//   const productosPorCajaGrande = Math.floor(pesoCajaGrande / capacidadRestante);
-//   const productosPorCajaMediana = Math.floor(pesoCajaMediana / capacidadRestante);
-
-//   return {
-//     cajasGrande: cantidadCajasGrande,
-//     cajasMediana: cantidadCajasMediana,
-//     productosPorCajaGrande,
-//     productosPorCajaMediana,
-//     camionSoportaPeso: true,
-//   };
-// }
-
-// // Ejemplo de uso
-// const capacidadMaximaCamion = 1000; // Capacidad máxima del camión en kg
-// const pesoDelPedido = 750; // Peso del pedido en kg
-
-// const resultado = calcularCajas(capacidadMaximaCamion, pesoDelPedido);
-// console.log(resultado);
+console.log(logistic(1500, 600, 5));
